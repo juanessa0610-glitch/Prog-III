c362349f6f1431a4cb56dd1d384a4d780578eb0d
Mora <juanessa0610@gmail.com>
2025-08-27 16:55:22 +0000

    Update

--- Clase 4/Biblioteca.java
+++ Clase 4/Biblioteca.java
@@ -2,7 +2,58 @@ public class Biblioteca{
     private String Titulo;
     private String Autor;
     private int Precio;
+    private int id;
 
-    public 
+    public Biblioteca(String titulo, String autor, int precio, int id) {
+        Titulo = titulo;
+        Autor = autor;
+        Precio = precio;
+        this.id = id;
+    }
+
+  
+
+public String getTitulo() {
+    return Titulo;
+}
+
+public void setTitulo(String titulo) {
+    Titulo = titulo;
+}
+
+public String getAutor() {
+    return Autor;
+}
+
+public void setAutor(String autor) {
+    Autor = autor;
+}
+
+public int getPrecio() {
+    return Precio;
+}
+
+public void setPrecio(int precio) {
+    Precio = precio;
+}
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+
+
+@Override
+public String toString() {
+    return "Biblioteca [Titulo=" + Titulo + ", Autor=" + Autor + ", Precio=" + Precio + ", id=" + id + "]";
+}  
 
 }
+
+
+
+    
---
+++ Clase 4/EjecutarLibro.java
@@ -0,0 +1,7 @@
+public class EjecutarLibro{
+
+    public static void main(String[] args) {
+        
+        Biblioteca Lib1 = new Biblioteca("", "", "", "")
+    }
+}
