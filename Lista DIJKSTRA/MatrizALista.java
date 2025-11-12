7fa15869988e5cf92692e5f16f8feebbb8e2911e
Mora <juanessa0610@gmail.com>
2025-11-12 17:29:22 +0000

    Matriz Update4

--- Lista DIJKSTRA/MatrizALista.java
+++ Lista DIJKSTRA/MatrizALista.java
@@ -9,20 +9,20 @@ public class MatrizALista {
             {0, 1, 1, 0}
         };
 
-        // La lista contendrá una lista enlazada por cada fila
+        
         List<LinkedList<Integer>> listaAdyacencia = new ArrayList<>(matriz.length);
 
-        // Transformar matriz → lista enlazada
+        
         for (int i = 0; i < matriz.length; i++) {
             LinkedList<Integer> nodos = new LinkedList<>();
             for (int j = 0; j < matriz[i].length; j++) {
-                if (matriz[i][j] == 1) // hay conexión
+                if (matriz[i][j] == 1) 
                     nodos.add(j);
             }
-            listaAdyacencia.add(nodos); // agregamos la lista enlazada del nodo i
+            listaAdyacencia.add(nodos); 
         }
 
-        // Mostrar el resultado
+        
         for (int i = 0; i < listaAdyacencia.size(); i++) {
             System.out.print((char)('A' + i) + " -> ");
             for (int nodo : listaAdyacencia.get(i))
